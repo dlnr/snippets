58d0eb4dac7bc78dd303264d859cdd7230dfa6d2
Niels Roozemond <niels@webenable.nl>
2018-02-20 09:50:03 +0100

    Moved to JS file

---
+++ agent.js
@@ -0,0 +1,23 @@
+var html = document.documentElement;
+var ie = false;
+var browser = navigator.userAgent.toLowerCase();
+html.classList.remove('no-js');
+html.className = 'js';
+
+if (browser.indexOf('msie') >= 0 || browser.indexOf('wow64') >= 0) {
+    html.className = 'js msie';
+    ie = true;
+}
+if (browser.indexOf('edge') >= 0) {
+    html.classList.add('edge');
+    ie = true;
+}
+if (browser.indexOf('iphone') >= 0) {
+    html.classList.add('iphone');
+}
+if (browser.indexOf('firefox') >= 0) {
+    html.classList.add('firefox');
+}
+if (browser.indexOf('safari') >= 0) {
+    html.classList.add('safari');
+}
